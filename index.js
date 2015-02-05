64886a21abbbc0ed1f69261cd84503fc9b633d70
ekmartin <ekmartinb@gmail.com>
2015-02-05 15:30:07 +0100

    Add check-env

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 var app = require('./lib/server');
+var checkEnv = require('check-env');
+
+checkEnv(
+  ['IRC_SERVER', 'BOT_NICK', 'CHANNEL_MAPPING', 'INCOMING_HOOK_URL', 'OUTGOING_HOOK_TOKEN']
+);
 
 app.listen(app.get('port'), function() {
   console.log('listening on %d', app.get('port'));
