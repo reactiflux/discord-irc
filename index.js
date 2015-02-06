0783d6aa520337e7081b4fb388912a75d87bf355
ekmartin <ekmartinb@gmail.com>
2015-02-06 11:50:21 +0100

    Use Winston for logging, fixes #3

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 var app = require('./lib/server');
+var logger = require('winston');
 var checkEnv = require('check-env');
 
 checkEnv(
   ['IRC_SERVER', 'BOT_NICK', 'CHANNEL_MAPPING', 'INCOMING_HOOK_URL', 'OUTGOING_HOOK_TOKEN']
 );
 
+if (process.env.NODE_ENV !== 'production') {
+  logger.level = 'debug';
+}
+
 app.listen(app.get('port'), function() {
-  console.log('listening on %d', app.get('port'));
+  logger.debug('Listening on port %d', app.get('port'));
 });
