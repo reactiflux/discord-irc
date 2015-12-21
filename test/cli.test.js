c92ad33be316e5652baba1c9711aeca651505674
ekmartin <mail@ekmartin.com>
2015-12-22 00:35:51 +0100

    Strip comments from JSON before parsing, fixes #11

--- test/cli.test.js
+++ test/cli.test.js
@@ -2,8 +2,8 @@
 import chai from 'chai';
 import sinon from 'sinon';
 import sinonChai from 'sinon-chai';
-import * as helpers from '../lib/helpers';
 import cli from '../lib/cli';
+import * as helpers from '../lib/helpers';
 import testConfig from './fixtures/test-config.json';
 import singleTestConfig from './fixtures/single-test-config.json';
 
@@ -31,10 +31,36 @@ describe('CLI', function() {
     this.createBotsStub.should.have.been.calledWith(testConfig);
   });
 
+  it('should strip comments from JSON config', function() {
+    process.env.CONFIG_FILE = process.cwd() + '/test/fixtures/test-config-comments.json';
+    process.argv = ['node', 'index.js'];
+    cli();
+    this.createBotsStub.should.have.been.calledWith(testConfig);
+  });
+
+  it('should support JS configs', function() {
+    process.env.CONFIG_FILE = process.cwd() + '/test/fixtures/test-javascript-config.js';
+    process.argv = ['node', 'index.js'];
+    cli();
+    this.createBotsStub.should.have.been.calledWith(testConfig);
+  });
+
+  it('should throw a ConfigurationError for invalid JSON', function() {
+    process.env.CONFIG_FILE = process.cwd() + '/test/fixtures/invalid-json-config.json';
+    process.argv = ['node', 'index.js'];
+    const wrap = () => cli();
+    (wrap).should.throw('The configuration file contains invalid JSON');
+  });
+
   it('should be possible to give the config as an option', function() {
     delete process.env.CONFIG_FILE;
-    process.argv = ['node', 'index.js',
-      '--config', process.cwd() + '/test/fixtures/single-test-config.json'];
+    process.argv = [
+      'node',
+      'index.js',
+      '--config',
+      `${process.cwd()}/test/fixtures/single-test-config.json`
+    ];
+
     cli();
     this.createBotsStub.should.have.been.calledWith(singleTestConfig);
   });
---
+++ test/fixtures/test-javascript-config.js
@@ -0,0 +1,20 @@
+module.exports = [
+  {
+    'nickname': 'test',
+    'server': 'irc.freenode.net',
+    'discordEmail': 'discord@test.com',
+    'discordPassword': 'whatapassword',
+    'channelMapping': {
+      '#discord': '#irc'
+    }
+  },
+  {
+    'nickname': 'test2',
+    'server': 'irc.freenode.net',
+    'discordEmail': 'discord@test.com',
+    'discordPassword': 'whatapassword',
+    'channelMapping': {
+      '#discord': '#irc'
+    }
+  }
+];
