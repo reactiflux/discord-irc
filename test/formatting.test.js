7ea41908c94d1dac437f4284e95a28e3f2f0d43d
Kevin Alberts <kevin@kevinalberts.nl>
2016-08-04 22:27:14 +0200

    Added formatting conversions to messages. Text formatting like bold, italics and underlining will now be converted between discord and IRC.

---
+++ test/formatting.test.js
@@ -0,0 +1,26 @@
+/* eslint-disable prefer-arrow-callback */
+
+import chai from 'chai';
+import { formatFromDiscordToIRC, formatFromIRCToDiscord } from '../lib/formatting';
+
+const expect = chai.expect;
+
+describe('formatFromDiscordToIRC', function() {
+  it('should parse a discord string into an irc string', function() {
+    const result = formatFromDiscordToIRC('**Hello!** *This* ~~is~~ __*a*__ __**discord**__ ' +
+      '__string__ __***with***__ *some* ***formatting.***');
+    expect(result).to.equal('\u0002Hello!\u000F \u001DThis\u000F is \u001F\u001Da\u000F ' +
+      '\u001F\u0002discord\u000F \u001Fstring\u000F \u001F\u001D\u0002with\u000F ' +
+      '\u001Dsome\u000F \u0002\u001Dformatting.\u000F');
+  });
+});
+
+describe('formatFromIRCToDiscord', function() {
+  it('should parse an irc string into a discord string', function() {
+    const result = formatFromIRCToDiscord('\u0002Hello!\u000F \u001DThis\u000F is ' +
+      '\u001F\u001Da\u000F \u001F\u0002discord\u000F \u001Fstring\u000F ' +
+      '\u001F\u001D\u0002with\u000F \u001Dsome\u000F \u0002\u001Dformatting.\u000F');
+    expect(result).to.equal('**Hello!** *This* is __*a*__ __**discord**__ __string__ ' +
+      '__***with***__ *some* ***formatting.***');
+  });
+});
