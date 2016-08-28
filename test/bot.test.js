a3b76d04d261affbb5a8e288cbc99855f53a297b
Christina Lena Korosec <christina.korosec1@gmail.com>
2016-08-28 12:22:49 +0200

    createServerStub as arrow function

--- test/bot.test.js
+++ test/bot.test.js
@@ -38,13 +38,11 @@ describe('Bot', function () {
     sandbox.restore();
   });
 
-  const createServerStub = function (nickname) {
-    return {
-      detailsOfUser() {
-        return { nick: nickname };
-      }
-    };
-  };
+  const createServerStub = (nickname = null) => ({
+    detailsOfUser() {
+      return { nick: nickname };
+    }
+  });
 
   it('should invert the channel mapping', function () {
     this.bot.invertedMapping['#irc'].should.equal('#discord');
