bc712281b8fe59f3a7af93317bcda18905e4e2be
Rahat Ahmed <rahatarmanahmed@gmail.com>
2017-03-25 13:28:56 -0500

    Add tests & catch reverse irc format

--- test/formatting.test.js
+++ test/formatting.test.js
@@ -3,23 +3,57 @@
 import chai from 'chai';
 import { formatFromDiscordToIRC, formatFromIRCToDiscord } from '../lib/formatting';
 
-const expect = chai.expect;
+chai.should();
 
-describe('Formatting', function () {
-  it('should parse a discord string into an irc string', function () {
-    const result = formatFromDiscordToIRC('**Hello!** *This* ~~is~~ __*a*__ __**discord**__ ' +
-      '__string__ __***with***__ *some* ***formatting.***');
-    expect(result).to.equal('\u0002Hello!\u0002 \u0016This\u0016 is \u001f\u0016a\u0016\u001f ' +
-      '\u001f\u0002discord\u0002\u001f \u001fstring\u001f \u001f\u0016\u0002with\u0002\u0016\u001f ' +
-      '\u0016some\u0016 \u0016\u0002formatting.\u0002\u0016');
+describe('Formatting', () => {
+  describe('Discord to IRC', () => {
+    it('should convert bold markdown', () => {
+      formatFromDiscordToIRC('**text**').should.equal('\x02text\x02');
+    });
+
+    it('should convert italic markdown', () => {
+      formatFromDiscordToIRC('*text*').should.equal('\x16text\x16');
+      formatFromDiscordToIRC('_text_').should.equal('\x16text\x16');
+    });
+
+    it('should convert underline markdown', () => {
+      formatFromDiscordToIRC('__text__').should.equal('\x1ftext\x1f');
+    });
+
+    it('should ignore strikethrough markdown', () => {
+      formatFromDiscordToIRC('~~text~~').should.equal('text');
+    });
+
+    it('should convert nested markdown', () => {
+      formatFromDiscordToIRC('**bold *italics***')
+      .should.equal('\x02bold \x16italics\x16\x02');
+    });
   });
 
-  it('should parse an irc string into a discord string', function () {
-    const result = formatFromIRCToDiscord('\u0002Hello!\u000F \u001DThis\u000F is ' +
-      '\u001F\u001Da\u000F \u001F\u0002discord\u000F \u001F\u000306string\u0003\u000F ' +
-      '\u001F\u001D\u0002with\u000F \u001D\u000304,01some\u0003\u000F ' +
-      '\u0002\u001Dformatting.\u000F');
-    expect(result).to.equal('**Hello!** *This* is __*a*__ __**discord**__ __string__ ' +
-      '__***with***__ *some* ***formatting.***');
+  describe('IRC to Discord', () => {
+    it('should convert bold IRC format', () => {
+      formatFromIRCToDiscord('\x02text\x02').should.equal('**text**');
+    });
+
+    it('should convert reverse IRC format', () => {
+      formatFromIRCToDiscord('\x16text\x16').should.equal('*text*');
+    });
+
+    it('should convert italic IRC format', () => {
+      formatFromIRCToDiscord('\x1dtext\x1d').should.equal('*text*');
+    });
+
+    it('should convert underline IRC format', () => {
+      formatFromIRCToDiscord('\x1ftext\x1f').should.equal('__text__');
+    });
+
+    it('should ignore color IRC format', () => {
+      formatFromIRCToDiscord('\x0306,08text\x03').should.equal('text');
+    });
+
+    it('should convert nested IRC format', () => {
+      formatFromIRCToDiscord('\x02bold \x16italics\x16\x02')
+      .should.equal('**bold *italics***');
+    });
   });
 });
