cb35b49e4c1c9487a9f2a290fed68b0221232f27
Rahat Ahmed <rahatarmanahmed@gmail.com>
2017-03-25 12:22:31 -0500

    Use irc-formatting for irc to discord formatting

--- test/formatting.test.js
+++ test/formatting.test.js
@@ -5,16 +5,16 @@ import { formatFromDiscordToIRC, formatFromIRCToDiscord } from '../lib/formattin
 
 const expect = chai.expect;
 
-describe('Formatting', function() {
-  it('should parse a discord string into an irc string', function() {
+describe('Formatting', function () {
+  it('should parse a discord string into an irc string', function () {
     const result = formatFromDiscordToIRC('**Hello!** *This* ~~is~~ __*a*__ __**discord**__ ' +
-      '__string__ __***with***__ *some* ***formatting.***');
+      '__string__ __***with***__ *some* _**formatting.**_');
     expect(result).to.equal('\u0002Hello!\u000F \u001DThis\u000F is \u001F\u001Da\u000F ' +
       '\u001F\u0002discord\u000F \u001Fstring\u000F \u001F\u001D\u0002with\u000F ' +
       '\u001Dsome\u000F \u0002\u001Dformatting.\u000F');
   });
 
-  it('should parse an irc string into a discord string', function() {
+  it('should parse an irc string into a discord string', function () {
     const result = formatFromIRCToDiscord('\u0002Hello!\u000F \u001DThis\u000F is ' +
       '\u001F\u001Da\u000F \u001F\u0002discord\u000F \u001F\u000306string\u0003\u000F ' +
       '\u001F\u001D\u0002with\u000F \u001D\u000304,01some\u0003\u000F ' +
