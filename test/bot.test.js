8a23a9193159b6167185af892e8e25ddfeab38cf
Fiaxhs <lowreg@gmail.com>
2017-07-28 11:54:02 +0200

    Fix avatar lookup function (and tests)

--- test/bot.test.js
+++ test/bot.test.js
@@ -836,34 +836,30 @@ describe('Bot', function () {
     const newConfig = { ...config, webhooks: { '#discord': 'https://discordapp.com/api/webhooks/id/token' } };
     const bot = new Bot(newConfig);
     bot.connect();
-    bot.sendToDiscord('Nick', '#irc', 'text');
     this.guild.members.set(1, { username: 'Nick', nickname: 'Different', avatarURL: 'avatarURL' });
-    this.bot.getDiscordAvatar('nick', '#irc').should.equal('avatarURL');
+    this.bot.getDiscordAvatar('Nick', '#irc').should.equal('avatarURL');
   });
 
   it('should find a matching username, case insensitive, when looking for an avatar', function () {
     const newConfig = { ...config, webhooks: { '#discord': 'https://discordapp.com/api/webhooks/id/token' } };
     const bot = new Bot(newConfig);
     bot.connect();
-    bot.sendToDiscord('Nick', '#irc', 'text');
     this.guild.members.set(1, { username: 'nick', nickname: 'Different', avatarURL: 'avatarURL' });
-    this.bot.getDiscordAvatar('nick', '#irc').should.equal('avatarURL');
+    this.bot.getDiscordAvatar('Nick', '#irc').should.equal('avatarURL');
   });
 
   it('should find a matching nickname, case sensitive, when looking for an avatar', function () {
     const newConfig = { ...config, webhooks: { '#discord': 'https://discordapp.com/api/webhooks/id/token' } };
     const bot = new Bot(newConfig);
     bot.connect();
-    bot.sendToDiscord('Different', '#irc', 'text');
     this.guild.members.set(1, { username: 'Nick', nickname: 'Different', avatarURL: 'avatarURL' });
-    this.bot.getDiscordAvatar('nick', '#irc').should.equal('avatarURL');
+    this.bot.getDiscordAvatar('Different', '#irc').should.equal('avatarURL');
   });
 
   it('should not return an avatar with two matching usernames when looking for an avatar', function () {
     const newConfig = { ...config, webhooks: { '#discord': 'https://discordapp.com/api/webhooks/id/token' } };
     const bot = new Bot(newConfig);
     bot.connect();
-    bot.sendToDiscord('common', '#irc', 'text');
     this.guild.members.set(1, { username: 'common', nickname: 'Different', avatarURL: 'avatarURL' });
     this.guild.members.set(2, { username: 'Nick', nickname: 'common', avatarURL: 'avatarURL' });
     chai.should().equal(this.bot.getDiscordAvatar('common', '#irc'), null);
