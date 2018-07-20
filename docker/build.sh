2dcd6c97fcb0ff244cf02b70cbb5a040d36039ed
Gareth Coles <gdude2002@gmail.com>
2018-07-20 09:52:23 +0100

    Fix dumb copy-paste mistake in build.sh

--- docker/build.sh
+++ docker/build.sh
@@ -1,18 +1,22 @@
 #!/bin/bash
 
 if [[ $TRAVIS_BRANCH == 'master' && $TRAVIS_PULL_REQUEST == 'false' ]]; then
-    echo "Connecting to docker hub"
-    echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-
     changed_lines=$(git diff HEAD~1 HEAD docker/Dockerfile | wc -l)
 
-    echo "Dockerfile was changed"
+    if [ $changed_lines != '0' ]; then
+        echo "Connecting to docker hub"
+        echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 
-    echo "Building..."
-    docker build -t reactiflux/discord-irc:latest -f docker/Dockerfile .
+        echo "Dockerfile was changed"
 
-    echo "Pushing image to Docker Hub..."
-    docker push reactiflux/discord-irc:latest
+        echo "Building..."
+        docker build -t reactiflux/discord-irc:latest -f docker/Dockerfile .
+
+        echo "Pushing image to Docker Hub..."
+        docker push reactiflux/discord-irc:latest
+    else
+      echo "Dockerfile was not changed, not building"
+    fi
 else
-    echo "Skipping deploy; Dockerfile was not changed"
+    echo "Skipping deploy; This is a PR or not on the master branch"
 fi
