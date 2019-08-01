5c18507ffdb822b23afdba1a15e40c7798077723
Gareth Coles <gdude2002@gmail.com>
2019-08-01 15:12:44 +0100

    Docker: Build for every commit

--- docker/build.sh
+++ docker/build.sh
@@ -1,22 +1,14 @@
 #!/bin/bash
 
 if [[ $TRAVIS_BRANCH == 'master' && $TRAVIS_PULL_REQUEST == 'false' ]]; then
-    changed_lines=$(git diff HEAD~1 HEAD docker/Dockerfile | wc -l)
+    echo "Connecting to docker hub"
+    echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 
-    if [ $changed_lines != '0' ]; then
-        echo "Connecting to docker hub"
-        echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+    echo "Building..."
+    docker build -t reactiflux/discord-irc:latest -f docker/Dockerfile .
 
-        echo "Dockerfile was changed"
-
-        echo "Building..."
-        docker build -t reactiflux/discord-irc:latest -f docker/Dockerfile .
-
-        echo "Pushing image to Docker Hub..."
-        docker push reactiflux/discord-irc:latest
-    else
-      echo "Dockerfile was not changed, not building"
-    fi
+    echo "Pushing image to Docker Hub..."
+    docker push reactiflux/discord-irc:latest
 else
     echo "Skipping deploy; This is a PR or not on the master branch"
 fi
