2c15bdb82f6c6b19165bb2c6aa9d0d15980d40e0
Isaac Aronson <i@pingas.org>
2023-08-25 19:25:23 -0500

    Fix dynamic import

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,10 @@
 #!/usr/bin/env -S deno run -A
 
 import { createBots } from './helpers.ts';
-import cli from './cli.ts';
 
 /* istanbul ignore next */
 if (import.meta.main) {
-  await cli();
+  await (await import('./cli.ts')).default();
 }
 
 export default createBots;
