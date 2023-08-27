8e6bb7ebde4bfa602b20153bcfe19c7d82f6c652
Isaac Aronson <i@pingas.org>
2023-08-27 10:58:15 -0500

    Remove node imports

--- lib/cli.ts
+++ lib/cli.ts
@@ -1,25 +1,23 @@
 #!/usr/bin/env -S deno run -A
 
-import fs from 'node:fs';
+import { join } from 'path';
 import { program } from 'npm:commander';
-import path from 'node:path';
 import * as helpers from './helpers.ts';
-import process from 'node:process';
 import { Config } from './config.ts';
 
-function run() {
+async function run() {
   program
     .option(
       '-c, --config <path>',
       'Sets the path to the config file, otherwise read from the env variable CONFIG_FILE.',
     )
-    .parse(process.argv);
+    .parse(Deno.args);
 
   const opts = program.opts();
 
   // Check if configFile.json exists in the current working directory
-  const localConfigFile = path.resolve(process.cwd(), 'config.json');
-  const localConfigExists = fs.existsSync(localConfigFile);
+  const localConfigFile = join(Deno.cwd(), 'config.json');
+  const localConfigExists = await helpers.exists(localConfigFile);
 
   // Determine the config file to use
   let configFile;
@@ -27,14 +25,18 @@ function run() {
     configFile = opts.config;
   } else if (localConfigExists) {
     configFile = localConfigFile;
-  } else if (process.env.CONFIG_FILE) {
-    configFile = process.env.CONFIG_FILE;
+  } else if (Deno.env.get('CONFIG_FILE')) {
+    configFile = Deno.env.get('CONFIG_FILE');
   } else {
     throw new Error('Missing environment variable CONFIG_FILE');
   }
 
-  const completePath = path.resolve(process.cwd(), configFile);
-  const config = JSON.parse(fs.readFileSync(completePath).toString()) as
+  const completePath = configFile.startsWith('/')
+    ? configFile
+    : join(Deno.cwd(), configFile);
+  const config = JSON.parse(
+    new TextDecoder().decode(Deno.readFileSync(completePath)),
+  ) as
     | Config
     | Config[];
   helpers.createBots(config);
